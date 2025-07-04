b5da7ecb9255e635284596b2d6c88dd30c6285ef
Mateo Villablanca <167587307+WhiteMooncy@users.noreply.github.com>
2025-07-04 02:01:14 -0400

    si

--- frontend/templates/form_login.php
+++ frontend/templates/form_login.php
@@ -4,36 +4,37 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../frontend/src/css/Style.css">
-    <title>Iniciar Sesión</title>
-</head>
-<body style="background-image: url(../../frontend/src/img/background_Cafeteria.png)">
-    <nav class="navbar">
-        <a href="index.php" class="navbar-brand">
-            <img src="../../frontend/src/icons/icon_cafe.png" alt="Logo"> Inicio
-        </a>
-    </nav>
-    <div class="login-container">
-        <h2>Iniciar Sesión</h2>
-        <?php
-        // Display login error message if set
-        if (isset($_SESSION['login_error'])) {
-            echo '<p style="color: red; text-align: center;">' . $_SESSION['login_error'] . '</p>';
-            unset($_SESSION['login_error']); // Clear the error after displaying
-        }
-        ?>
-        <form method="post" action="../../backend/php/conexion/login.php">
-            <label for="username">Nombre de Usuario:</label>
-            <input type="text" name="username" id="username" required>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../../frontend/src/css/styleDashboard.css">
+        <title>Iniciar Sesión</title>
+    </head>
+    <body id="login-page">
+        <nav class="navbar">
+            <a href="index.php" class="navbar-brand">
+                <img src="../../frontend/src/icons/icon_cafe.png" alt="Logo"> Inicio
+            </a>
+        </nav>
+        <div class="login-container">
+            <h2>Iniciar Sesión</h2>
+            <?php
+            // Display login error message if set
+            if (isset($_SESSION['login_error'])) {
+                echo '<p style="color: red; text-align: center;">' . $_SESSION['login_error'] . '</p>';
+                unset($_SESSION['login_error']); // Clear the error after displaying
+            }
+            ?>
+            <form method="post" action="../../backend/php/conexion/login.php">
+                <label for="username">Nombre de Usuario:</label>
+                <input type="text" name="username" id="username" required>
 
-            <label for="password">Contraseña:</label>
-            <input type="password" name="password" id="password" required>
+                <label for="password">Contraseña:</label>
+                <input type="password" name="password" id="password" required>
 
-            <button type="submit">Iniciar Sesión</button>
-        </form>
-    </div>
-</body>
+                <button type="submit">Iniciar Sesión</button>
+                <p class="register-link">¿No tienes cuenta? requistrate Aqui</p>
+            </form>
+        </div>
+    </body>
 </html>
